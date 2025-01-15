ac357965a1bc641d187333a5b032c5a423020ae9
Bryan <bryan@itsthedevman.com>
2025-01-14 19:07:58 -0500

    Initial

---
+++ test/test_sortsmith.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestSortsmith < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Sortsmith::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
