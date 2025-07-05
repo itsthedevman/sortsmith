e05f93e3d2e15b9c49d008307f07e05ed91fa897
Bryan <bryan@itsthedevman.com>
2025-07-05 17:52:19 -0400

    refactor(sorter): simplify apply_step method by using a signature array for method calls

--- lib/sortsmith/sorter.rb
+++ lib/sortsmith/sorter.rb
@@ -86,9 +86,16 @@ module Sortsmith
     end
 
     def apply_step(step, item_a, item_b)
+      signature =
+        if (arguments = step[:arguments]) && arguments.size > 0
+          [step[:method], *arguments]
+        else
+          [step[:method]]
+        end
+
       [
-        item_a.public_send(step[:method]),
-        item_b.public_send(step[:method])
+        item_a.public_send(*signature),
+        item_b.public_send(*signature)
       ]
     end
 
