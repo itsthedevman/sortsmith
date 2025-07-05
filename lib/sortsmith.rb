042dd7b7d56b31fc1a481cb9223915f923c8c4fc
Bryan <bryan@itsthedevman.com>
2025-07-05 17:12:54 -0400

    feat(enumerable): add custom sort_by method to integrate Sortsmith::Sorter test(enumerable): add tests for custom sort_by method behavior chore(test): create test files for Enumerable and Sorter classes

--- lib/sortsmith.rb
+++ lib/sortsmith.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "sortsmith/version"
-require_relative "sortsmith/core_ext/enumerator"
+require_relative "sortsmith/core_ext/enumerable"
 require_relative "sortsmith/sorter"
 
 module Sortsmith
---
+++ test/sortsmith/test_sorter.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestSorter < Minitest::Test
+  def test_testing
+    assert(true)
+  end
+end
